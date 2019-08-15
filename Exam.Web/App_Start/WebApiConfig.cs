35d1c58cef4509dbbaf9417bdd17f075347b8aae
niuhongkui <397208778@qq.com>
2019-08-15 15:02:39 +0800

    5

--- Exam.Web/App_Start/WebApiConfig.cs
+++ Exam.Web/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace Exam.Web
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
